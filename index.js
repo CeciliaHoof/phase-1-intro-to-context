521d7f7495e5f72f51bd4b3956c3fe4ceb105ef8
Cecilia <ccchimento@gmail.com>
2024-06-17 07:56:54 -0600

    lab complete

--- index.js
+++ index.js
@@ -43,3 +43,42 @@ const createTimeOutEvent = (obj, dateStr) => {
     return obj
 }
 
+const hoursWorkedOnDate = (obj, dateStr) => {
+    const timeIn = obj.timeInEvents.filter(event => event.date === dateStr)[0].hour
+    const timeOut = obj.timeOutEvents.filter(event => event.date === dateStr)[0].hour
+
+    return (timeOut - timeIn)/100
+}
+
+// const cRecord = createEmployeeRecord(["Julius", "Caesar", "General", 1000])
+// createTimeInEvent(cRecord, "0044-03-15 0900")
+// createTimeOutEvent(cRecord, "0044-03-15 1100")
+// hoursWorkedOnDate(cRecord, "0044-03-15")
+
+const wagesEarnedOnDate = (obj, dateStr) => {
+    return hoursWorkedOnDate(obj, dateStr) * obj.payPerHour
+}
+
+const allWagesFor = (obj) => {
+    const dates = obj.timeInEvents.map(event => event.date)
+    
+    const allWages = dates.map(d => wagesEarnedOnDate(obj, d))
+
+    let acum = 0
+    for(let i = 0; i < allWages.length; i++){
+        acum += allWages[i]
+    }
+
+    return acum
+}
+
+const calculatePayroll = (arr) => {
+    const payrollArr = arr.map(employee => allWagesFor(employee))
+
+    let acum = 0
+    for(let i = 0; i < payrollArr.length; i++){
+        acum += payrollArr[i]
+    }
+
+    return acum
+}
